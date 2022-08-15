dc5c37fbdf3282b0b9c7acce6c149f8bc0a7abce
Twizzy <109283077+Twizzycat@users.noreply.github.com>
2022-08-15 23:12:36 +0200

    Add files via upload

---
+++ js/social.js
@@ -0,0 +1,4 @@
+const discord = () => (window.location.href = "https://discords.com/bio/p/kramos");
+const instagram = () => (window.location.href = "https://instagram.com/kramos.oz");
+const steam = () => (window.location.href = "https://steamcommunity.com/id/7lh/");
+document.body.style.backgroundColor = `#000001`;
